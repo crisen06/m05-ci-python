dca887eb6a34cefe29a62436f2130bf6637ce346
crisen06 <crisen.cordova@gmail.com>
2026-05-12 02:47:45 +0000

    prueba3

--- tests/test_calculadora.py
+++ tests/test_calculadora.py
@@ -54,4 +54,5 @@ class TestDividir:
     def test_division_por_cero_lanza_excepcion(self):
         with pytest.raises(ValueError, match="No se puede dividir entre cero"):
             dividir(5, 0)
+   
             
